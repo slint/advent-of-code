10906ca1004dd9ff1f2dc6d39e2efc44915cbdfe
Alexander Ioannidis <a.ioannidis@cern.ch>
2017-12-02 18:31:11 +0100

    tests: add mypy

--- 1/captcha.py
+++ 1/captcha.py
@@ -6,23 +6,22 @@ with open(Path(__file__).parent / 'input.txt') as fp:
     puzzle_input = fp.read().strip()
 
 
-def captcha(s: str, step: int):
-    s = str(s)
+def captcha(s: str, step: int) -> int:
     return sum(int(n) for i, n in enumerate(s) if s[(i + step) % len(s)] == n)
 
 def test_captcha_sum():
-    assert captcha(1122, 1) == 3
-    assert captcha(1111, 1) == 4
-    assert captcha(1234, 1) == 0
-    assert captcha(91212129, 1) == 9
+    assert captcha('1122', 1) == 3
+    assert captcha('1111', 1) == 4
+    assert captcha('1234', 1) == 0
+    assert captcha('91212129', 1) == 9
 
 
 def test_captcha_step():
-    assert captcha(1212, len('1212') // 2) == 6
-    assert captcha(1221, len('1221') // 2) == 0
-    assert captcha(123425, len('123425') // 2) == 4
-    assert captcha(123123, len('123123') // 2) == 12
-    assert captcha(12131415, len('12131415') // 2) == 4
+    assert captcha('1212', len('1212') // 2) == 6
+    assert captcha('1221', len('1221') // 2) == 0
+    assert captcha('123425', len('123425') // 2) == 4
+    assert captcha('123123', len('123123') // 2) == 12
+    assert captcha('12131415', len('12131415') // 2) == 4
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Advent of Code 2017 Solutions
 
-These are my solutions for the
+My Python 3.6 solutions for the
 [Advent of Code 2017 Calendar](http://adventofcode.com/2017).
 
 **Personal leaderboard code**: `197364-6c22136b`
+
+## Tests
+
+Preferrably in a new virtualenv:
+
+```console
+# mvirtualenv aoc2017 -p python3.6
+pip install -r requirements.txt
+pytest
+```
