6e3702711fb6843b48871d69094b7588668b1679
Alexander Ioannidis <a.ioannidis@cern.ch>
2021-12-05 15:18:43 +0100

    Solve the two first days

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,7 +1,11 @@
 use std::collections::HashMap;
 
+mod d1;
+mod d2;
+
 pub fn get_solutions() -> HashMap<u8, fn(String) -> ()> {
     let mut solutions: HashMap<u8, fn(String) -> ()> = HashMap::new();
-    // solutions.insert(N, dN::run);
+    solutions.insert(1, d1::run);
+    solutions.insert(2, d2::run);
     solutions
 }
